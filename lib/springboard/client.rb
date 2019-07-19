e4903d83ea41bcca139006616007139b99a333e8
labocho <labocho@penguinlab.jp>
2019-07-19 17:55:01 +0900

    initial commit

---
+++ lib/springboard/client.rb
@@ -0,0 +1,8 @@
+require "springboard/client/version"
+
+module Springboard
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/springboard/client/version.rb
@@ -0,0 +1,5 @@
+module Springboard
+  module Client
+    VERSION = "0.1.0"
+  end
+end
